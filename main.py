3bf049d83c0e4c6c6475acb7e5adf72e8423b9f7
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-07-31 20:59:15 -0300

    Puxando Informações de Forma Dinâmica do Python para o Site

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask , render_template
 
 app = Flask(__name__)
 
+lista_usuarios = ['Lucas','Lice','Sivaldo','Vanusa','Gute']
+
 @app.route("/")
 def home():
     return render_template('home.html')#render retorna tudo da pagina home.html
@@ -10,6 +12,10 @@ def home():
 def contato():
     return render_template('contato.html')
 
+
+@app.route("/usuarios")
+def usuarios():                         #o primeiro paramentro( lista_usuarios) vai para a pagina html
+    return render_template('usuarios.html', lista_usuarios=lista_usuarios)#o segundo é a variavel da lista
 
 if __name__ == "__main__":
     app.run(debug=True)
