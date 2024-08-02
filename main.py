8131831400581a18a7fe2d3f9e439468fc5740b7
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-08-02 11:39:35 -0300

    Padronizando links do site

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-from flask import Flask , render_template
+from flask import Flask , render_template, url_for
 
 app = Flask(__name__)
 
 lista_usuarios = ['Lucas','Lice','Sivaldo','Vanusa','Gute']
 
-@app.route("/")
+@app.route('/')
 def home():
     return render_template('home.html')#render retorna tudo da pagina home.html
 
-@app.route("/contato")
+@app.route('/contato')
 def contato():
     return render_template('contato.html')
 
 
-@app.route("/usuarios")
+@app.route('/usuarios')
 def usuarios():                         #o primeiro paramentro( lista_usuarios) vai para a pagina html
     return render_template('usuarios.html', lista_usuarios=lista_usuarios)#o segundo é a variavel da lista
 
