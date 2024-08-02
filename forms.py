9852330e74992f9cac8b69de7018eb50a3090778
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-08-02 19:42:44 -0300

    Criando Formulários do Site

---
+++ forms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class FormCriarConta(FlaskForm):
+    username = StringField("Nome do Usuário", validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    confirmacao_senha = PasswordField('Confirmação de senha', validators=[DataRequired(), EqualTo('senha')])
+    botao_submit_criarConta = SubmitField('Criar Conta')
+
+
+class FormaLogin(FlaskForm):
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    botao_submit_login = SubmitField('Fazer Login')
+
+
+
+    #
+
