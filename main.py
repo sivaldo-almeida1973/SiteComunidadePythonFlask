c876ec2005ff3ac6b42ec3f975ef34863583c33a
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-08-03 19:54:12 -0300

    csrf token e Mensagem de Sucesso nos Formulários

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template, url_for
+from flask import Flask , render_template, url_for, request, flash, redirect
 from forms import  FormaLogin, FormCriarConta
 
 app = Flask(__name__)
@@ -26,8 +26,20 @@ def usuarios():                         #o primeiro paramentro( lista_usuarios) 
 @app.route('/login', methods=['GET', 'POST']) #pagina de login
 def login():
     form_login = FormaLogin()  #criar instancias e passa
-    form_criaConta =  FormCriarConta()      #depois disso jogar para dentro do site no login.html
-    return render_template('login.html', form_login=form_login, form_criarConta=form_criaConta)
+    form_criarConta =  FormCriarConta()      #depois disso jogar para dentro do site no login.html
+
+    if form_login.validate_on_submit() and 'botao_submit_login' in request.form:  #validando formularios
+        #fez login com sucesso exibir msg de login bem sucedido
+        flash(f'Login feito com sucesso! {form_login.email.data}', 'alert-success')# data -e o que foi preenchido no campo email
+        #redirecionar para homepage  (importar flash, redirect)
+        return redirect(url_for('home'))
+
+    if form_criarConta.validate_on_submit() and 'botao_submit_criarConta' in request.form:
+         flash(f'Conta criada para o email! {form_criarConta.email.data}',  'alert-success')
+        #criou conta com sucesso
+         #redireciona , depois disso ajeita no base.html para exibir msg
+         return redirect(url_for('home'))
+    return render_template('login.html', form_login=form_login, form_criarConta=form_criarConta)
 
 
 if __name__ == "__main__":
